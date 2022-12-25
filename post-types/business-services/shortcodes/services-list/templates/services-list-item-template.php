b43e5ab152a7bfc480fc6fe7e2adaf47e862eb9d
thebrandstudio <allan@bstt.mx>
2022-12-24 19:16:40 -0600

    Update services-list-item-template.php

--- post-types/business-services/shortcodes/services-list/templates/services-list-item-template.php
+++ post-types/business-services/shortcodes/services-list/templates/services-list-item-template.php
@@ -30,7 +30,7 @@ $value = get_field( "descripcion", get_the_ID() );
         </div>
         <div class="mkdf-bsl-item-bottom-holder clearfix">
             <?php if (!empty($description)) : ?>
-                <div id="descripcion" class="mkdf-bsl-item-description-holder">
+                <div id="descripcion" class="mkdf-bsl-item-description-holder" style="list-style-type: circle;">
                     <p><?php echo $description; ?></p>
                     <p><?php echo $value; ?></p>
                 </div>
